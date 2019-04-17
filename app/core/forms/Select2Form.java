cc0eed080a024bfcebf098c65d9586411ddcfb88
Felipe Bonezi <felipebonezi@gmail.com>
2019-04-17 17:04:34 -0300

    Select2 with String id.

--- app/core/forms/Select2Form.java
+++ app/core/forms/Select2Form.java
@@ -2,12 +2,16 @@ package core.forms;
 
 public class Select2Form {
 
-    public Long id;
+    public String id;
     public String text;
 
     public Select2Form() {}
 
     public Select2Form(Long id, String text) {
+        this(id.toString(), text);
+    }
+
+    public Select2Form(String id, String text) {
         this.id = id;
         this.text = text;
     }
